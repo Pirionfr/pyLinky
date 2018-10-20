fe40cb85c1363124fc4a0d556aa6f53f9fa0f70b
Dimitri Capitaine <dimitri.capitaine@corp.ovh.com>
2018-10-20 15:54:55 +0200

    change distribution type on tags

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
       name='pylinky',
-      version='0.1.7',
+      version='0.1.8',
       description='Get your consumption data from your Enedis account (www.enedis.fr)',
       long_description=long_description,
       author='Dimitri Capitaine',
