e5d141f9cb67d4c6c8b4a3072837007c7bb26f36
Ludovic Rousseau <ludovic.rousseau@free.fr>
2018-12-08 15:39:03 +0100

    Add the GNU GPL v3 licence to the source code files

--- pylinky/__init__.py
+++ pylinky/__init__.py
@@ -1,1 +1,18 @@
+#    pylink: get your consumption data from your enedis account
+#    copyright (c) 2018  pirionfr, https://github.com/pirionfr
+#    copyright (c) 2018  ludovic rousseau, <ludovic.rousseau@free.fr>
+#
+#    this program is free software: you can redistribute it and/or modify
+#    it under the terms of the gnu general public license as published by
+#    the free software foundation, either version 3 of the license, or
+#    (at your option) any later version.
+#
+#    this program is distributed in the hope that it will be useful,
+#    but without any warranty; without even the implied warranty of
+#    merchantability or fitness for a particular purpose.  see the
+#    gnu general public license for more details.
+#
+#    you should have received a copy of the gnu general public license
+#    along with this program.  if not, see <https://www.gnu.org/licenses/>.
+
 from pylinky.client import LinkyClient
